d32ab54a50a25b87cca21013e99f36471756b2a2
VlV <92279938+VlV-515@users.noreply.github.com>
2023-07-13 14:05:24 -0600

    Feat⚙️Add navbar and footer

--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [],
-  imports: [CommonModule, MatFormFieldModule, MatInputModule],
-  exports: [MatFormFieldModule, MatInputModule],
+  imports: [CommonModule, MatFormFieldModule, MatInputModule, MatTooltipModule],
+  exports: [MatFormFieldModule, MatInputModule, MatTooltipModule],
 })
 export class MaterialModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './material.module';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [NavbarComponent, FooterComponent],
   imports: [CommonModule, MaterialModule],
-  exports: [MaterialModule],
+  exports: [MaterialModule, NavbarComponent, FooterComponent],
 })
 export class SharedModule {}
